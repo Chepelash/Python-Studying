d240f3fcf1234d03815f026c491210c009abfe0f
Chepelash <chepelash@gmail.com>
2017-03-30 22:26:23 +0300

    pep8_me.py completed

--- Python2/Lesson_1/homework/pep8_me.py
+++ Python2/Lesson_1/homework/pep8_me.py
@@ -1,29 +1,46 @@
 import random
 import string
+import os
 
 
-def create_file(namef,dir,size):
-        if(size.isdigit()!=True):
-                if size.endswith('KB')==True:
-                        s1 = size.split('KB')
-                        size1 = int(s1[0])*1024
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('MB')==True:
-                        s1 = size.split('MB')
-                        size1 = int(s1[0])*1048567
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('GB') == True:
-                        s1 = size.split('GB')
-                        size1 = int(s1[0]) * 1073741824
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
+def create_file(namef, path, size):
+    if not size.isdigit():
+        if size.endswith('KB'):
+            s1 = size.split('KB')
+            size1 = int(s1[0]) * 1024
+            token = ''.join(random.choice(string.ascii_uppercase +
+                                          string.ascii_lowercase +
+                                          string.digits) for _ in range(size1))
+        elif size.endswith('MB'):
+            s1 = size.split('MB')
+            size1 = int(s1[0])*1048567
+            token = ''.join(random.choice(string.ascii_uppercase +
+                                          string.ascii_lowercase +
+                                          string.digits) for _ in range(size1))
+        elif size.endswith('GB'):
+            s1 = size.split('GB')
+            size1 = int(s1[0]) * 1073741824
+            token = ''.join(random.choice(string.ascii_uppercase +
+                                          string.ascii_lowercase +
+                                          string.digits) for _ in range(size1))
         else:
-                token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(int(size)))
+            size1 = int(size[:-1])
+            token = ''.join(random.choice(string.ascii_uppercase +
+                                          string.ascii_lowercase +
+                                          string.digits) for _ in range(size1))
 
-        file = open(dir+namef,"w")
-        file.write(token)
+    else:
+        token = ''.join(random.choice(string.ascii_uppercase +
+                                      string.ascii_lowercase +
+                                      string.digits) for _ in range(int(size)))
+
+    file_path = os.path.join(namef, path)
+    file = open(file_path, "w")
+    file.write(token)
+    file.close()
 
 
-create_file("/test1.txt","E:",'10KB')
-create_file("/test2.txt","E:",'1024')
-create_file("/test11.txt","E:",'2MB')
-create_file("/test21.txt","E:",'1B')
+create_file("test1.txt", "F:\\torrent", '10KB')
+create_file("test2.txt", "F:\\torrent", '1024')
+create_file("test11.txt", "F:\\torrent", '2MB')
+create_file("test21.txt", "F:\\torrent", '1B')
